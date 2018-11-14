15b6d1265bcec2cd21504628a1685d62a48eae14
Alice1998 <alicerdfz@163.com>
2018-11-14 09:10:37 +0800

    keyboard layout #Alice

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -9,60 +9,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<sys/types.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
-#include<netinet/ip.h>
-#include<netinet/ip_icmp.h>
-#include<sys/time.h>
-#include<arpa/inet.h>
-#include<net/route.h>
 #include <iostream>
-#include <errno.h>
 #include <vector>
-#include <sstream>
 using namespace std;
+*/
 
-void split(const string& s,vector<string>& sv,const char flag = ' ') {
-    sv.clear();
-    istringstream iss(s);
-    string temp;
-    
-    while (getline(iss, temp, flag)) {
-        sv.push_back(temp);
-    }
-    return;
-}
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    string input="no ip route 192.168.1.0/24 192.168.2.254\n";
-    printf("%s",input.c_str());
-    vector<string> sv;
-    split(input, sv, ' ');
-    for (const auto& s : sv) {
-        cout << s << endl;
-    }
-    vector<string> second;
-    split(sv[3],second,'/');
-    for (const auto& s : second) {
-        cout << s << endl;
-    }
-    stringstream sstr1(second[1]);
-    int x;
-    sstr1 >> x;
-    printf("%d",x);
-    return 0;
-}
- */
 #include "CImg.h"
 using namespace cimg_library;
 int main()
 {
-    // 定义一个每个颜色 8 位(bit)的 640x400 的彩色图像
-    CImg<unsigned char> img(640,400,1,3);
+    CImg<unsigned char> SrcImg;
+    SrcImg.load_bmp("test.bmp");
+    int width = SrcImg.width();
+    int height = SrcImg.height();
+    // 定义一个每个颜色 8 位(bit)的 640x400 的彩色图像 横 纵
+    CImg<unsigned char> img(width+200,height+200,1,3);
     //将像素值设为 0（黑色）
     img.fill(0);
     // 定义一个紫色
@@ -71,7 +32,10 @@ int main()
     // 在坐标(100, 100)处画一个紫色的“Hello world”
     img.draw_text(100,100,"Hello World",purple);
     // 在一个标题为“My first CImg code”的窗口中显示这幅图像
-    img.display("My first CImg code");
+
+    img.draw_image(100,100,SrcImg);
+    img.display("keyboard Layout");
+    //img.display("keyboard Layout");
     
     return 0;
 } 
